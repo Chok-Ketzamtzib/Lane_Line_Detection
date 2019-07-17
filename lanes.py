238d5a638b0c186267df680947a778870922186c
Chok-Ketzamtzib <wakeforest.ww@gmail.com>
2019-07-17 01:04:10 -0400

    Optimized Code. Added average_slope_intercept and make_coordinates functions to create smoother lanes on lane_image and final combined_image

--- lanes.py
+++ lanes.py
@@ -8,6 +8,31 @@ import cv2
 import numpy as np
 #import matplotlib.pyplot as plt
 
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return np.array([x1, y1, x2, y2])
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis = 0)
+    right_fit_average = np.average(right_fit, axis = 0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
 """
 Canny Function
 Converts the input image to grayscale, and then runs
@@ -15,7 +40,7 @@ the image through the Canny functino, which
 includes the GaussianBlur function. 
 The GaussianBlur function call is redudancy. 
 
-"""
+"""    
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5),0)  
@@ -25,32 +50,43 @@ def canny(image):
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2= line.reshape(4)
+        for x1, y1, x2, y2 in lines:
             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10) #line(image, first points, end points, tuple of RGB color of line, pixel thickness) 
     return line_image
        
 def region_of_interest(image):
     height = image.shape[0]
     polygons = np.array([
-    [(200, height), (1100, height), (550,250)]
+    [(200, height), (1100, height), (650,50)]
     ])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
     masked_image = cv2.bitwise_and(image, mask) #takes original image and masks them based on mask
     return masked_image
     
-image = cv2.imread('White-Broken-1.jpg')
+image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
-canny = canny(lane_image)
-cropped_image = region_of_interest(canny)
+canny_image = canny(lane_image)
+cropped_image = region_of_interest(canny_image)
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5) #2nd and 3rd args represent size of bins for creating line of best fit from Hough space. 4th argument is threshold; 
 #Threshold is minimum number of votes/intersections needed to detect a line; 5th artgument is placceholder array (empty); 
-line_image =  display_lines(lane_image, lines)
+averaged_lines = average_slope_intercept(lane_image, lines)
+line_image =  display_lines(lane_image, averaged_lines)
 combined_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-cv2.imshow('result', line_image)
+"""
+plt.imshow(lane_image)
+plt.show()
+cv2.imshow('Original Image', image)
+cv2.waitKey(1000)
+cv2.imshow('Canny Image', canny_image)
+cv2.waitKey(1000)
+cv2.imshow('Region of Interest', region_of_interest(image))
+cv2.waitKey(1000)
+cv2.imshow('Lane Lines Detected', line_image)
+cv2.waitKey(1000)
+"""
 cv2.imshow('Final Result', combined_image)
-cv2.imshow("lol", canny)
+#cv2.waitKey(1000)
 cv2.waitKey(0)
 
 
